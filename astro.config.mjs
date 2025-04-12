4537e817e6060b1b8c21fb6e8ebb106f2400ba17
Dafaild25 <154031943+Dafaild25@users.noreply.github.com>
2025-04-12 13:37:41 -0500

    configuracion en astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
     output: 'static',
-    base: '/portafolio-astro/',
+    base: isDev ? '/' : '/portafolio-astro/',
     integrations: [tailwind()],
 });
